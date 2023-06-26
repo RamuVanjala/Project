3a1d45e87e193818c4951336165e18c818244971
Vanjala <ramuvanjala1101@gmail.com>
2023-06-26 16:59:56 +0530

    practising

---
+++ index.js/testing js.html/index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+   
+    <h1 id="Ramu">"My frnd name is Mahi"</h1>
+    <h1 id="Mahi"></h1>
+    <script>
+        console.log('Hello');
+        
+    </script>
+</body>
+</html>
